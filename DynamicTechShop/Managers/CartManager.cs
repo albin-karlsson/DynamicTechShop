6511759482cf9fa727e898412ce11333a9652e45
albin-karlsson <55614148+albin-karlsson@users.noreply.github.com>
2024-02-21 10:35:54 +0100

    Finish app

---
+++ DynamicTechShop/Managers/CartManager.cs
@@ -0,0 +1,14 @@
+﻿using DynamicTechShop.Models;
+
+namespace DynamicTechShop.Managers
+{
+    public static class CartManager
+    {
+        public static List<ProductModel> Cart { get; set; } = new();
+
+        public static double? GetTotalPrice()
+        {
+            return Cart.Sum(p => p.Price);
+        }
+    }
+}
