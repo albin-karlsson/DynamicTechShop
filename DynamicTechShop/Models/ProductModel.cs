1ce20166f7df778b0b85c2fcef7a80d0b2013c60
albin-karlsson <55614148+albin-karlsson@users.noreply.github.com>
2024-02-21 09:41:45 +0100

    Initial commit

---
+++ DynamicTechShop/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DynamicTechShop.Models
+{
+    public class ProductModel
+    {
+        public int? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Category { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public double? Price { get; set; }
+    }
+}
